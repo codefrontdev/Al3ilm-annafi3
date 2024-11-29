34f82835010bcfb669b4b8597bb71cc9315be167
codefrontdev <cd62970@gmail.com>
2024-11-29 19:21:35 +0100

    first commit

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use("*", (req, res, next) => {
 app.use(globalErrorMiddleware);
 
 // run listen
-app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log("listening on port 3000");
 });
 
