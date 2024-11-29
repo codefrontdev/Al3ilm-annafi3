2f8bec1a0cfd26a844e3617f1cf48f6bfeb797cd
codefrontdev <cd62970@gmail.com>
2024-11-29 19:30:40 +0100

    first commit

--- middleware/ErrorMiddleware.js
+++ middleware/ErrorMiddleware.js
@@ -1,11 +1,13 @@
 const globalErrorMiddleware = (err, req, res, next) => {
+  // تعيين الحالة الافتراضية للخطأ
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+  err.status = err.status || "error";
 
-  if (process.env.NODE_ENV !== 'production') { 
+  // إذا كانت البيئة تطويرية، يتم إرسال خطأ شامل مع التفاصيل
+  if (process.env.NODE_ENV !== "production") {
     sendErrorForDev(err, res);
   } else {
-    sendErrorForProd(err, res)
+    sendErrorForProd(err, res);
   }
 };
 
@@ -19,9 +21,20 @@ const sendErrorForDev = (err, res) => {
 };
 
 const sendErrorForProd = (err, res) => {
-  return res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
+  let { statusCode, status, message } = err;
+
+  // التعامل مع أخطاء معينة قد تكون غير آمنة أو تحتاج إلى معالجة خاصة
+  if (err.isOperational === false) {
+    // من الأفضل تسجيل الأخطاء غير التشغيلية بحيث يمكن معالجة الحالات الاستثنائية
+    console.error("ERROR 💥:", err);
+    statusCode = 500;
+    status = "error";
+    message = "Something went very wrong!";
+  }
+
+  return res.status(statusCode).json({
+    status,
+    message,
   });
 };
 
